1984d32648a7db94eb7cab640573864a5a5c0158
a <keliauskas0328@gmail.com>
2024-09-18 10:53:58 +0300

    buttons

--- app/src/main/java/com/example/helloworlfapp/MainActivity.java
+++ app/src/main/java/com/example/helloworlfapp/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.helloworlfapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Find the TextView and Button by their IDs
+        TextView myTextView = findViewById(R.id.textView);
+        Button myButton = findViewById(R.id.button);
+
+        // Set an OnClickListener for the button
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the sentence to display in the TextView
+                myTextView.setText("Didelis suris");
+            }
         });
     }
 }
